143a6bf3da048548dddd14e7dfd1cb1752ccad4d
lou-dy <louise.dyquiangco@gmail.com>
2018-02-21 02:19:24 -0700

    4.3 Controllers - added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+  root 'simple_pages#landing_page'
+
   get 'simple_pages/about'
 
   get 'simple_pages/contact'
