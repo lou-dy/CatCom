dfc02caa53e3134b5fc5f7d13185eb265ecf1123
lou-dy <louise.dyquiangco@gmail.com>
2018-03-06 11:57:04 -0700

    5.1 redirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to "simple_pages/landing_page" }
+        format.html { redirect_back fallback_location: @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
