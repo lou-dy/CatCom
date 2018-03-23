26b1cdb0a63cf5fd0d38ddb4da25138b4d6ef3d5
lou-dy <louise.dyquiangco@gmail.com>
2018-03-23 08:07:57 -0700

    contact form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,4 +6,14 @@ class SimplePagesController < ApplicationController
 
   def index
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'louise.dyquiangco@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
 
+  post 'simple_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
