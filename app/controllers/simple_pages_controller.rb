b41199c559031383739c784a492490c954dcee50
lou-dy <louise.dyquiangco@gmail.com>
2018-06-01 16:17:49 -0700

    mailer edit

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -9,7 +9,7 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
+      to: 'louise.dyquiangco@gmail.com',
       subject: "A new contact form message from #{@name}",
       body: @message).deliver_now
   end
