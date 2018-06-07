05274d90b3e20725f77202e0e5a502cfa78e5703
lou-dy <louise.dyquiangco@gmail.com>
2018-06-07 14:17:20 -0700

    rspec test files

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ end
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+  gem 'rspec-rails', '~> 3.7'
 end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe MaleBettum do
+
+  context "rating average" do
+
+    let(:male_betta) { MaleBettum.create!(name: "Test fish", price: "15") }
+    let(:user) { User.create!(first_name:"John", last_name:"Doe", email:"johndoe@test.com", password:"123456")}
+
+    before do
+      male_betta.comments.create!(rating: 1, user: user, body: "Awful fish!")
+      male_betta.comments.create!(rating: 3, user: user, body: "Ok fish!")
+      male_betta.comments.create!(rating: 5, user: user, body: "Great fish!")
+    end
+
+    it "returns the average rating of all comments" do
+      expect(male_betta.average_rating).to eq 3
+    end
+  end
+
+  context "betta validation" do
+    let(:male_betta) { MaleBettum.new(description: "Nice fish")}
+
+    it "should not be valid" do
+      expect(MaleBettum.new(description: "Nice bike")).not_to be_valid
+    end
+  end
+
+end
