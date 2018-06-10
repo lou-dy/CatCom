f6411550327d1c5f1be7d0c2fedb0201cac416c2
lou-dy <louise.dyquiangco@gmail.com>
2018-06-10 13:25:13 -0700

    http status

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -27,6 +27,7 @@ describe UsersController, type: :controller do
     context 'when a user is not logged in' do
       it 'redirects to log in' do
         get :show, params: { id: user.id }
+        expect(response).to have_http_status(302)
         expect(response).to redirect_to(new_user_session_path)
       end
     end
